c4bebd5a290579373499620e2d93eb5fe270ae51
CedricNOGUERA <cedric.noguera@lwane.com>
2022-05-11 14:51:16 -1000

    data json

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,44 @@ import {
 import "./App.css";
 
 function App() {
+
+const userList = [
+  {
+    id: 1,
+    first_name: "Mark",
+    last_name: "Otto",
+    username: "mark@otto",
+    role: "admin",
+    pass: "Om**2020", 
+  },
+  {
+    id: 2,
+    first_name: "Greg",
+    last_name: "Ratto",
+    username: "greg@ratto",
+    role: "els",
+    pass: "Gr**2020", 
+  },
+  {
+    id: 3,
+    first_name: "Sam",
+    last_name: "Sotto",
+    username: "sam@sotto",
+    role: "manager",
+    pass: "Ss**2020", 
+  },
+  {
+    id: 4,
+    first_name: "Drax",
+    last_name: "Dantes",
+    username: "drax@dantes",
+    role: "frist_els",
+    pass: "Dd**2020", 
+  }
+]
+
+
+
   return (
     <Container fluid className="App px-0">
       <Container fluid className="px-0">
@@ -49,7 +87,20 @@ function App() {
             </tr>
           </thead>
           <tbody>
+            {userList.map((user: any, indx: any) => (
             <tr>
+              <td>{user.id}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.username}</td>
+              <td>{user.role}</td>
+              <td>{user.pass}</td>
+           
+              <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/pencil.png" alt="icon"/></Button></td>
+              <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/delete-sign.png" alt="delete" /></Button></td>
+            </tr>
+            ))}
+            {/* <tr>
               <td>1</td>
               <td>Mark</td>
               <td>Otto</td>
@@ -68,17 +119,7 @@ function App() {
               <td>Om**2020</td>
               <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/pencil.png" alt="icon"/></Button></td>
               <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/delete-sign.png" alt="delete" /></Button></td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>Admin</td>
-              <td>Om**2020</td>
-              <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/pencil.png" alt="icon"/></Button></td>
-              <td><Button className="bg-light border-light"><img src="https://img.icons8.com/stickers/25/delete-sign.png" alt="delete" /></Button></td>
-            </tr>
+            </tr> */}
            
           </tbody>
         </Table>
