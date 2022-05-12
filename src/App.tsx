09bd46e073c990cdcc0373b59b866ff6f0d51516
CedricNOGUERA <cedric.noguera@lwane.com>
2022-05-11 22:27:45 -1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import {
   Button,
+  Col,
   Container,
   Form,
   FormControl,
   InputGroup,
   Modal,
   Navbar,
+  Row,
   Table,
 } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  const [userData, setUserData] = React.useState<any>([])
+  const [userData, setUserData] = React.useState<any>([]);
   const userList = [
     {
       id: 1,
@@ -55,44 +57,123 @@ function App() {
   const [show2, setShow2] = useState(false);
   const handleClose2 = () => setShow2(false);
   const handleShow2 = () => setShow2(true);
-  
-  const [fName, setFName] = useState('');
-  const [lName, setLName] = useState('');
-  const [role, setRole] = useState('');
-  const [pass, setPass] = useState('');
-  
 
+  const [id, setId] = useState(0);
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [role, setRole] = useState("");
+  const [pass, setPass] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  //GET
   useEffect(() => {
-    setUserData(userList)
-  }, [])
-
-  function handleUpdateUser(todo: any) {
-    handleShow()
-
-  }
+    setUserData(userList);
+  }, []);
+  
   //POST
-  const addUser =(e: any) => {
-    e.preventDefault()
-    const userName = fName.toLowerCase() + "@"+ lName.toLowerCase()
-    const result =  {
+  const addUser = (event: any) => {
+    event.preventDefault()
+    const userName = fName.toLowerCase() + "@" + lName.toLowerCase();
+    const result = {
       id: Math.random(),
       first_name: fName,
       last_name: lName,
       username: userName,
       role: role,
       pass: pass,
-
+    };
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
     }
-    setUserData([...userData, result])
-
-
-   console.log(userName)
-   
-  } 
+    setUserData([...userData, result]);
+    setValidated(true);
+    
+    handleClose2();
+    setFName("");
+    setLName("");
+    setRole("");
+    setPass("");
+  };
   
-  console.log(userData)
-  console.log(userList)
+  //UPDATE
+    function handleUpdateUser(user: any) {
+      handleShow();
 
+      setId(user.id)
+      setFName(user.first_name)
+      setLName(user.last_name)
+      setRole(user.role)
+      setPass(user.pass)
+    }
+    const updateUser = (event: any) => {
+      event.preventDefault()
+      const userName = fName.toLowerCase() + "@" + lName.toLowerCase();
+      const result = {
+        id: id,
+        first_name: fName,
+        last_name: lName,
+        username: userName,
+        role: role,
+        pass: pass,
+      };
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      const index = userData.findIndex((user: any) => user.id === id)
+      const newTab = [...userData]
+      newTab[index] = result
+      
+      setUserData(newTab);
+      setValidated(true);
+      
+      handleClose();
+      setId(0)
+      setFName("");
+      setLName("");
+      setRole("");
+      setPass("");
+    };
+
+
+
+
+
+  // const addUser2 = (e: any) => {
+  //   const userName = fName.toLowerCase() + "@" + lName.toLowerCase();
+  //   const result = {
+  //     id: Math.random(),
+  //     first_name: fName,
+  //     last_name: lName,
+  //     username: userName,
+  //     role: role,
+  //     pass: pass,
+  //   };
+  //   const form = e.currentTarget;
+  //   if (form.checkValidity() === false) {
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //   } else {
+  //     setValidated(true);
+  //   }
+  //   if (validated) {
+  //     setUserData([...userData, result]);
+
+  //     handleClose2();
+  //     setFName("");
+  //     setLName("");
+  //     setRole("");
+  //     setPass("");
+  //     console.log(userName);
+  //   }
+  // };
+
+
+  console.log(userData);
+  console.log(userList);
 
   return (
     <Container fluid className="App px-0">
@@ -109,16 +190,7 @@ function App() {
           </Container>
         </Navbar>
       </Container>
-      <Container fluid className="px-0">
-        <InputGroup className="mb-3 px-5">
-          <InputGroup.Text id="basic-addon1"></InputGroup.Text>
-          <FormControl
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-      </Container>
+      <Container fluid className="px-0" style={{ height: "15vh" }}></Container>
       <Container>
         <Table striped bordered hover>
           <thead>
@@ -135,43 +207,42 @@ function App() {
           </thead>
           <tbody>
             {userData.map((user: any, indx: any) => (
-              <tr keky={user.id}>
+              <>
+              <tr key={user.name}>
                 <td>{user.id}</td>
                 <td>{user.first_name}</td>
                 <td>{user.last_name}</td>
                 <td>{user.username}</td>
                 <td>{user.role}</td>
                 <td>{user.pass}</td>
-
-                <td>
-                  <Button className="bg-light border-light" onClick={handleUpdateUser}>
+                <td><Button
+                    className="bg-light border-light"
+                    onClick={(e) => handleUpdateUser(user) }
+                    >
                     <img
                       src="https://img.icons8.com/stickers/25/pencil.png"
                       alt="update"
-                    />
-                  </Button>
-                </td>
-                <td>
-                  <Button className="bg-light border-light">
+                      />
+                  </Button></td>
+                <td><Button className="bg-light border-light">
                     <img
                       src="https://img.icons8.com/stickers/25/delete-sign.png"
                       alt="delete"
-                    />
-                  </Button>
+                    /></Button>
                 </td>
               </tr>
+            </>
             ))}
           </tbody>
           <Button onClick={handleShow2} className="bg-light border-light m-2">
             <img
               src="https://img.icons8.com/stickers/25/plus-math.png"
               alt="add user"
-            ></img>
-          </Button>{" "}
+              ></img>
+          </Button>
           Add an user
         </Table>
       </Container>
-
 
       <Modal
         show={show}
@@ -183,48 +254,77 @@ function App() {
           <Modal.Title>New user</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+        <Form  onSubmit={updateUser}>
+            <Row className="mb-3">
             <Form.Group className="mb-3" controlId="formBasicFirstName">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" value={userList[0].first_name }/>
-              <Form.Text className="text-muted">
-              </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter first name"
+                value={fName}
+                onChange={(e) => setFName(e.currentTarget.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+              <Form.Control.Feedback type="invalid">
+                    Please provide your name.
+                  </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicLastName">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" placeholder="Enter last name" />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicUserName">
-              <Form.Label>username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicRole">
-              <Form.Label>Role</Form.Label>
-              <Form.Control type="text" placeholder="Enter role" />
-              <Form.Text className="text-muted">
-              </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter last name"
+                value={lName}
+
+                onChange={(e) => setLName(e.currentTarget.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+              <Form.Control.Feedback type="invalid">
+                    Please provide your Last name.
+                  </Form.Control.Feedback>
             </Form.Group>
 
+            <Form.Select
+              required
+              aria-label="Default select example"
+              value={role}
+              onChange={(e) => setRole(e.currentTarget.value)}
+            >
+              <option value="">Rôle</option>
+              <option value="admin">Admin</option>
+              <option value="first_els">Emloyé principal</option>
+              <option value="els">E.L.S</option>
+            </Form.Select>
+            </Row>
+            <Row className="mb-3">
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                value={pass}
+
+                onChange={(e) => setPass(e.currentTarget.value)}
+              />
             </Form.Group>
-            
-            {/* <Button variant="primary" type="submit">
-              Submit
-            </Button> */}
+
+             
+            </Row>
+        
+            <Button type="submit">Submit</Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
+        {/* <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose} type="submit">Submit</Button>
-        </Modal.Footer>
+          <Button variant="primary" onClick={handleClose} type="submit">
+            Submit
+          </Button>
+        </Modal.Footer> */}
       </Modal>
       <Modal
         show={show2}
@@ -236,48 +336,64 @@ function App() {
           <Modal.Title>New user</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={addUser}>
+        <Form  onSubmit={addUser}>
+            <Row className="mb-3">
             <Form.Group className="mb-3" controlId="formBasicFirstName">
               <Form.Label>First name</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" onChange={(e) => setFName(e.currentTarget.value)}/>
-              <Form.Text className="text-muted">
-              </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter first name"
+                onChange={(e) => setFName(e.currentTarget.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+              <Form.Control.Feedback type="invalid">
+                    Please provide your name.
+                  </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicLastName">
               <Form.Label>Last name</Form.Label>
-              <Form.Control type="text" placeholder="Enter last name" onChange={(e) => setLName(e.currentTarget.value)}/>
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            {/* <Form.Group className="mb-3" controlId="formBasicUserName">
-              <Form.Label>username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" onChange={(e) => setFName(e.currentTarget.value)}/>
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group> */}
-            <Form.Group className="mb-3" controlId="formBasicRole">
-              <Form.Label>Role</Form.Label>
-              <Form.Control type="text" placeholder="Enter role" onChange={(e) => setRole(e.currentTarget.value)}/>
-              <Form.Text className="text-muted">
-              </Form.Text>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter last name"
+                onChange={(e) => setLName(e.currentTarget.value)}
+              />
+              <Form.Text className="text-muted"></Form.Text>
+              <Form.Control.Feedback type="invalid">
+                    Please provide your Last name.
+                  </Form.Control.Feedback>
             </Form.Group>
 
+            <Form.Select
+              required
+              aria-label="Default select example"
+              onChange={(e) => setRole(e.currentTarget.value)}
+            >
+              <option value="">Rôle</option>
+              <option value="admin">Admin</option>
+              <option value="first_els">Emloyé principal</option>
+              <option value="els">E.L.S</option>
+            </Form.Select>
+            </Row>
+            <Row className="mb-3">
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" onChange={(e) => setPass(e.currentTarget.value)}/>
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPass(e.currentTarget.value)}
+              />
             </Form.Group>
-            
-            <Button variant="primary" type="submit" onClick={handleClose2}>
-              Submit
-            </Button>
+
+             
+            </Row>
+        
+            <Button type="submit">Submit</Button>
           </Form>
+         
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose2}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose2} type="submit">Submit</Button>
-        </Modal.Footer>
       </Modal>
     </Container>
   );
